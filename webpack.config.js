ca4c91bcf4c576725ab769f04c838b4b277a7974
Tyree <tyreewhite01102006@gmail.com>
2023-06-22 11:49:06 -0500

    Style initial load page

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
  const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
    mode: 'development',
@@ -7,11 +6,6 @@
      index: './src/index.js',
    },
   devtool: 'inline-source-map',
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Development',
-     }),
-   ],
    output: {
      filename: 'main.js',
      path: path.resolve(__dirname, 'dist'),
